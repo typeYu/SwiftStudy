b492406b7b015fc81d2bff0d56a62b0147762764
Kiyong Kim <scgyong@gmail.com>
2022-07-11 15:23:58 +0900

    Image Switcher

---
+++ ImageSwitcher/ImageSwitcher/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ImageSwitcher
+//
+//  Created by Kiyong Kim on 2022/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "arrowtriangle.left")
+                        .font(.largeTitle)
+                }
+                Spacer()
+                Text("1 / 5")
+                Spacer()
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "arrowtriangle.right")
+                        .font(.largeTitle)
+                        .aspectRatio(contentMode: .fit)
+                }
+            }
+            Image("cat_1")
+                .resizable()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
