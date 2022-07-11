f6b40240d98a45b91d946c5a72b8b417a3a448ee
Kiyong Kim <scgyong@gmail.com>
2022-07-11 17:28:31 +0900

    day1 Playground..

--- day1/Playground/MyPlayground.playground/Contents.swift
+++ day1/Playground/MyPlayground.playground/Contents.swift
@@ -1,14 +1,40 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
-var a = 10
-a + 30
+func greet(name: String, day: String) -> String {
+    return "Hello \(name), today is \(day)"
+}
 
-a += 1
+func hello(a: String, b: String) -> String {
+    return a + b
+}
 
-let arr = [ "1", "b", "c" ]
+var fff = greet
 
-//arr[2] = "hello"
-arr.append("Hello")
+let mm = fff("Kim", "Sunday")
 
-arr
+fff = hello
+
+let m2 = fff("Kim", "Sunday")
+
+ff
+
+let msg1 = greet(name: "TUKorea", day: "Monday")
+//let msg1 = greet("TUKorea", "Monday")
+let msg2 = greet(name: "x", day: "good")
+
+let t1 = (10, 20)
+t1.0
+t1.1
+
+let t2 = (x:10, y:20)
+t2.x
+t2.y
+
+
+
+func sum_mul(a: Int, b: Int) -> (Int, Int) {
+    return (a+b, a*b)
+}
+
+let x = sum_mul(a: 10, b: 20)
+let y = sum_mul(a: 100, b: 102)
