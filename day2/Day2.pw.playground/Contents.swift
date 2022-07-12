db9dedcfb6f52d16e401a500f69e888953023f1b
Kiyong Kim <scgyong@gmail.com>
2022-07-12 15:19:08 +0900

    enum

--- day2/Day2.pw.playground/Contents.swift
+++ day2/Day2.pw.playground/Contents.swift
@@ -1,3 +1,36 @@
 import Foundation
 
-var greeting = "Hello, playground"
+enum Rank: Int { // RawType
+    case ace = 1, two, three, four, five
+    case six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    func desc() -> String {
+        switch self {
+        case .ace:
+            return "Ace"
+        case .jack: return "Jack"
+        case .queen: return "Queen"
+        case .king: return "King"
+        default:
+            return "\(self.rawValue)"
+        }
+    }
+}
+
+var r = Rank.ten
+//r = .seven
+r.rawValue
+
+r.desc()
+Rank.nine.desc()
+
+enum Response {
+    case result(String, String)
+    case error(String)
+}
+
+let res1 = Response.error("Not ready")
+let res2 = Response.result("Kim", "Seoul")
+
+
