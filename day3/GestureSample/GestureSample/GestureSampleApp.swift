84b4712aa8b300a14900b0e9175e3e251d2b2098
Kiyong Kim <scgyong@gmail.com>
2022-07-13 14:01:58 +0900

    GestureSample skeleton

---
+++ day3/GestureSample/GestureSample/GestureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureSampleApp.swift
+//  GestureSample
+//
+//  Created by Kiyong Kim on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct GestureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
