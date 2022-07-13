c6c50810acd3dbfb3da18d090784a78901786500
Kiyong Kim <scgyong@gmail.com>
2022-07-13 16:35:24 +0900

    MemoryGame Skeleton

---
+++ day4/MemoryGame/MemoryGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by Kiyong Kim on 2022/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
