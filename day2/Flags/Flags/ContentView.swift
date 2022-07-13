5707cfa3d00fb10cc0fa4bea649c0f35fb8f5f50
Kiyong Kim <scgyong@gmail.com>
2022-07-13 11:53:43 +0900

    NavigationView 적용

--- day2/Flags/Flags/ContentView.swift
+++ day2/Flags/Flags/ContentView.swift
@@ -9,15 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {
-            ForEach(Nations.contintents, id: \.region) { continent in
-                Section(header: Text(continent.region)) {
-                    ForEach(continent.countries, id: \.name) { c in
-                        CountryView(country: c)
+        NavigationView {
+            List {
+                ForEach(Nations.contintents, id: \.region) { continent in
+                    Section(header: Text(continent.region)) {
+                        ForEach(continent.countries, id: \.name) { c in
+                            NavigationLink(destination: {
+                                Text(c.name)
+                            }) {
+                                CountryView(country: c)
+                            }
+                        }
                     }
                 }
             }
-        }.listStyle(SidebarListStyle())
+            .listStyle(SidebarListStyle())
+            .navigationBarTitle("Countries")
+        }
     }
 }
 
@@ -49,6 +57,12 @@ struct CountryView: View {
 
 struct CountryView_Previews: PreviewProvider {
     static var previews: some View {
-        CountryView(country: Nations.contintents[0].countries[0])
+        List {
+            CountryView(country: Nations.contintents[0].countries[0])
+            CountryView(country: Nations.contintents[0].countries[1])
+        }
+//        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+//            CountryView(country: Nations.contintents[0].countries[item])
+//        }
     }
 }
