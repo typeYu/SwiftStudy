87d7b8e900f6b8f6b0fb8dfc8278935a2b09cb10
Kiyong Kim <scgyong@gmail.com>
2022-07-15 12:53:24 +0900

    MapSample skeleton

---
+++ day5/MapSample/MapSample/MapSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSampleApp.swift
+//  MapSample
+//
+//  Created by Kiyong Kim on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct MapSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
