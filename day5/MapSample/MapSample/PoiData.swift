9bbbd36bd97b2eeb7c7d748a54d241cecc889b00
Kiyong Kim <scgyong@gmail.com>
2022-07-15 14:11:32 +0900

    BaseURL & Key

--- day5/MapSample/MapSample/PoiData.swift
+++ day5/MapSample/MapSample/PoiData.swift
@@ -21,5 +21,12 @@ class PoiItem: Codable {
 }
 
 class PoiData: ObservableObject {
-    
+    @Published var rows = [PoiItem]()
+    struct Const {
+        static let baseUrl = "https://openapi.gg.go.kr/PlaceThatDoATasteyFoodSt"
+        static let key = "e92ec3e6a9f44981974d4746d28f8995"
+    }
+    func load() {
+        let strUrl = "\(Const.baseUrl)?Type=json&Key=\(Const.key)"
+    }
 }
